4235d3b1289865a6518b4530c4f7a178490510fe
tjburns08 <burns.tyler@gmail.com>
2018-09-21 15:32:36 +0200

    Changed the data to the Samusik Mouse Bone Marrow dataset

--- R/data.R
+++ R/data.R
@@ -1,15 +1,16 @@
-#' Publically available dataset from Andreas Gruetzkau at Deutsches Rheuma
-#' Forschung Zentrum (DRFZ)
-#' Flow Repository ID FR-FCM-ZZWC
-#' @format a tibble of 10000 cells by 49 features
-"nnvis_cells"
+#' Publically available dataset from Nikolay Samusik of the Nolan Lab,
+#' consisting of mouse bone marrow. Original data is 86,864 cells, but here
+#' it is subsampled down to 10,000. The data were asinh transformed. The
+#' Flow Repository ID FR-FCM-ZZPH.
+#' @format a tibble of 10000 cells by 54 features
+"samusik_cells"
 
 #' Selected surface markers from the dqvis_cells dataset to be used for dim
 #' reduction analysis
-#' @format a vector of 29 features
-"nnvis_surface_markers"
+#' @format a vector of 38 features
+"samusik_surface_markers"
 
 #' The output of t-SNE results from the dqvis_cells dataset with the
 #' dqvis_surface_markers used as input.
 #' @format a tibble of 10000 cells by 2 features
-"nnvis_tsne"
+"samusik_tsne"
